c4ac4d07a136732a1ae1abc5f24c35753f7cd457
Amira Veliz <amira.veliz@gmail.com>
2025-06-13 00:33:56 -0300

    adding api call to get weather information by city

---
+++ src/api/WeatherApi.js
@@ -0,0 +1,30 @@
+const API_KEY = "78d41e726878449887713230251306";
+const URL = "http://api.weatherapi.com/v1/current.json";
+
+const WeatherApi = {
+    getWeatherByCity: async (city) => {
+        try {
+            const params = new URLSearchParams({
+                key: API_KEY,
+                q: city,
+            });
+            const response = await fetch(`${URL}?${params.toString()}`);
+            const data = await response.json();
+            if (!response.ok) {
+                throw new Error(
+                    data.error || "Failed to fetch weather information"
+                );
+            }
+
+            return {
+                data: data.data,
+            };
+        } catch (error) {
+            return {
+                error: error.message,
+            };
+        }
+    },
+};
+
+export default WeatherApi;
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,32 @@
 "use client";
 
+import WeatherApi from "@/api/WeatherApi";
 import { useState } from "react";
+import { Loader } from "lucide-react";
 
 export default function Home() {
     const [city, setCity] = useState("");
+    const [error, setError] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const [weather, setWeather] = useState();
 
-    const handleSubmit = (event) => {
+    const handleSubmit = async (event) => {
         event.preventDefault();
-        console.log(city);
+
+        setIsLoading(true);
+        setError("");
+
+        const response = await WeatherApi.getWeatherByCity(city);
+
+        if (response.error) {
+            setError(error);
+        } else {
+            setWeather(response);
+        }
+
+        setIsLoading(false);
+
+        console.log(weather);
     };
 
     return (
@@ -19,7 +38,7 @@ export default function Home() {
             <form
                 type="submit"
                 onSubmit={handleSubmit}
-                className="max-w-5xl flex flex-col md:flex-row items-baseline gap-3 md:gap-2 mx-auto w-full p-5 md:p-8"
+                className="max-w-5xl flex flex-col md:flex-row items-center gap-3 md:gap-2 mx-auto w-full p-5 md:p-8"
             >
                 <div className="flex flex-col w-full md:w-[70%]">
                     <input
@@ -29,12 +48,20 @@ export default function Home() {
                         placeholder="Enter a city or country..."
                         required
                         onChange={(event) => setCity(event.target.value)}
+                        disabled={isLoading}
                     />
                 </div>
 
                 <div className="flex flex-col w-full md:w-[30%]">
                     <button className="bg-primary-100 hover:bg-primary-100/90 border-primary-100 border-1 disabled:bg-gray-400/30 p-2 rounded-md text-white cursor-pointer text-xs md:text-[medium]">
-                        SEARCH
+                        {isLoading ? (
+                            <Loader
+                                className="animate-spin mx-auto my-0"
+                                size={20}
+                            />
+                        ) : (
+                            "SEARCH"
+                        )}
                     </button>
                 </div>
             </form>
